e0eba5a17f8f3e1fe9b509097092206d67cc9e1e
Ufuk Mehmet Atıcı <ufuuk.atc1@gmail.com>
2024-07-01 17:43:38 +0300

    Dashboard Hazırlığına Başlandı Arayüz Oluşturuldu

--- RealEstate_Dapper_UI/Controllers/ProductController.cs
+++ RealEstate_Dapper_UI/Controllers/ProductController.cs
@@ -44,5 +44,27 @@ namespace RealEstate_Dapper_UI.Controllers
             ViewBag.v = categoryValues;
             return View();
         }
+
+        public async Task<IActionResult> ProductDealOfTheDayStatusChangeToFalse(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:44327/api/Products/ProductDealOfTheDayStatusChangeToFalse/" + id);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public async Task<IActionResult> ProductDealOfTheDayStatusChangeToTrue(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:44327/api/Products/ProductDealOfTheDayStatusChangeToTrue/" + id);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
     }
 }

6449b67ad2f23a610ffe0ca2b5432ca9f4a5f18d
Ufuk Mehmet Atıcı <ufuuk.atc1@gmail.com>
2024-07-04 12:42:34 +0300

    Property Single Sayfa

--- RealEstate_Dapper_UI/Controllers/PropertyController.cs
+++ RealEstate_Dapper_UI/Controllers/PropertyController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using RealEstate_Dapper_UI.Dtos.ProductDetailDtos;
 using RealEstate_Dapper_UI.Dtos.ProductDtos;
 
 namespace RealEstate_Dapper_UI.Controllers
@@ -29,6 +30,35 @@ namespace RealEstate_Dapper_UI.Controllers
         [HttpGet]
         public async Task<IActionResult> PropertySingle(int id)
         {
+            id = 1;
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:44327/api/Products/GetProductByProductId?id=" + id);
+            var jsonData = await responseMessage.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<ResultProductDto>(jsonData);
+
+            var client2 = _httpClientFactory.CreateClient();
+            var responseMessage2 = await client2.GetAsync("https://localhost:44327/api/ProductDetails/GetProductDetailByProductId?id=" + id);
+            var jsonData2 = await responseMessage2.Content.ReadAsStringAsync();
+            var values2 = JsonConvert.DeserializeObject<GetProductDetailByIdDto>(jsonData2);
+
+
+            ViewBag.title1 = values.title;
+            ViewBag.price = values.price;
+            ViewBag.city = values.city;
+            ViewBag.district = values.district;
+            ViewBag.address = values.address;
+            ViewBag.type = values.type;
+
+            ViewBag.productSize = values2.productSize;
+            ViewBag.bedRoomCount = values2.bedRoomCount;
+            ViewBag.bathCount = values2.bathCount;
+            ViewBag.roomCount = values2.roomCount;
+            ViewBag.garageSize = values2.garageSize;
+            ViewBag.buildYear = values2.buildYear;
+            ViewBag.location = values2.location;
+            ViewBag.videoUrl = values2.videoUrl;
+
+
             return View();
         }
     }
